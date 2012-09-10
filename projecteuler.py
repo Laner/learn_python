7c5bb9be1e596d5824ec0e51e80ecddeb6be0dc2
Laner <runar.dankel@gmail.com>
2012-09-10 15:31:32 +0300

    added problem 5,6 and 7

--- projecteuler.py
+++ projecteuler.py
@@ -68,4 +68,33 @@ for x in reversed(range(900,999)):
                 if nr == int(str(nr)[::-1]):
                         print nr
                         break
-        
+
+"""
+Problem 5
+30 November 2001
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+"""
+"""
+Problem 6
+14 December 2001
+
+The sum of the squares of the first ten natural numbers is,
+
+12 + 22 + ... + 102 = 385
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)2 = 552 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025  385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+"""
+"""
+Problem 7
+28 December 2001
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime numbe
+"""
