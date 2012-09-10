0d238a6e303c0c8a133996cecc08ecdde34b88c1
Laner <runar.dankel@gmail.com>
2012-09-10 14:58:12 +0300

    Added problem 4 description

--- projecteuler.py
+++ projecteuler.py
@@ -53,3 +53,12 @@ def findPrimeFactors(total, prime=2):
 		findPrimeFactors(total, nextPrime(prime))
 
 print(findPrimeFactors(600851475143))
+
+"""
+Problem 4
+16 November 2001
+
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+"""
