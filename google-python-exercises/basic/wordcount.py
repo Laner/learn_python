d342a98f6d043dbc7de7b6c7702cac564857736b
Runar Dankel <runar.dankel@gmail.com>
2012-09-16 20:30:06 +0200

    Print words working

--- google-python-exercises/basic/wordcount.py
+++ google-python-exercises/basic/wordcount.py
@@ -44,29 +44,31 @@ import sys
 # You could write a helper utility function that reads a file
 # and builds and returns a word/count dict for it.
 # Then print_words() and print_top() can just call the utility function.
+
 def print_words(filename):
-  f = open(filename, 'r')
-  count = {}
-  for word in f:
-    print word
+    f = open(filename, 'rU').read().lower().strip().split()
+    word_dict = {}
+    for word in f:
+      if word in word_dict:
+        word_dict[word] += 1
+      else:
+        word_dict[word] = 1
+    # print word_dict
 ###
 
 # This basic command line argument parsing code is provided and
 # calls the print_words() and print_top() functions which you must define.
 def main():
-  if len(sys.argv) != 3:
-    print 'usage: ./wordcount.py {--count | --topcount} file'
-    sys.exit(1)
-
   option = sys.argv[1]
-  filename = sys.argv[2]
-  if option == '--count':
-    print_words(filename)
-  elif option == '--topcount':
-    print_top(filename)
-  else:
-    print 'unknown option: ' + option
-    sys.exit(1)
+    if len(sys.argv) != 3:
+        print 'usage: ./wordcount.py {--count | --topcount} file'
+        sys.exit(1)
+
+    option = sys.argv[1]
+    filename = sys.argv[2]
+    if option == '--count':
+    else:
+        sys.exit(1)
 
 if __name__ == '__main__':
-  main()
+    main()
