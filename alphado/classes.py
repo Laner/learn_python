5c4419d668e01eab7ac67ebf02bc26dd45d9acc2
OpenShift guest <runar.dankel@gmail.com>
2012-11-26 08:30:40 -0500

    output working

--- alphado/classes.py
+++ alphado/classes.py
@@ -1,9 +1,16 @@
+items = []
+projects = []
+
 class Item(object):
     def __init__(self, parent, id, text, status = "none"):
         self.parent = parent
         self.id = id
         self.text = text
         self.status = status
+        items.append(self)
+        
+    def __repr__(self):
+        return str(self.text)
     
     def print_item(self):
         print self.parent, self.id, self.text, self.status
@@ -13,6 +20,10 @@ class Project(object):
         self.id = id
         self.text = text
         self.status = status
+        projects.append(self)
+    
+    def __repr__(self):
+        return str(self.text)
         
     def print_item(self):
         print self.id, self.text, self.status
@@ -20,10 +31,15 @@ class Project(object):
 one = Item("B", "a", "Ta ut matavfallet", "none")
 three = Project("A", "Regnskap", "none")
 two = Project("B", "Husarbeid")
+four = Item("B", "b", "Rydde")
 one.print_item()
 two.print_item()
 three.print_item()
 print "----------------------------"
+print items
 
-
-for one.parent in 
+for project in projects:
+    print project, ":"
+    for item in items:
+        if item.parent == project.id:
+            print "- " + item.text
